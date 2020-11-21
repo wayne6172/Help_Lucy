ffe9399ddd521599d5b7670ba7652e91edbf83be
wayne <wayne8692wayne8692@@gmail.com>
2020-11-21 11:02:46 +0800

    add Linklist.c

---
+++ LinkList.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int value;
+    struct node *next;
+}Node;
+
+int main(){
+    Node *headPtr = NULL, *newNode;
+    int n;
+
+    while(1){
+        scanf("%d", &n);
+
+        if(n == -1)
+            break;
+
+        newNode = (Node *)malloc(sizeof(Node));
+        newNode->value = n;
+
+        newNode->next = headPtr;
+        headPtr = newNode;
+    }
+
+    
+
+}
